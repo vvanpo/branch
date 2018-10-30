0b896504edf7c6e820b39ef097e2bce05f1f1afd
Victor van Poppelen <vvanpo@gmail.com>
2018-10-29 23:25:02 -0700

    Replace serial dataype with uuid

--- schema.sql
+++ schema.sql
@@ -1,56 +1,56 @@
 
-CREATE TABLE contact (
-	id serial PRIMARY KEY,
-	email text NOT NULL UNIQUE,
-	name text NOT NULL,
-	registered timestamp(0) with time zone NOT NULL DEFAULT now()
-);
-
-CREATE TABLE contact_group (
-	id serial PRIMARY KEY,
+CREATE TABLE permission (
+	id uuid PRIMARY KEY,
 	name text NOT NULL UNIQUE
-);
-
-CREATE TABLE contact_group_map (
-	id serial PRIMARY KEY,
-	contact integer NOT NULL REFERENCES contact,
-	contact_group integer NOT NULL REFERENCES contact_group,
-	UNIQUE (contact, contact_group)
 );
 
 CREATE TABLE role (
-	id serial PRIMARY KEY,
+	id uuid PRIMARY KEY,
 	name text NOT NULL UNIQUE
 );
 
-CREATE TABLE permission (
-	id serial PRIMARY KEY,
-	name text NOT NULL UNIQUE
-);
-
-CREATE TABLE role_permission_map (
-	id serial PRIMARY KEY,
-	role integer NOT NULL REFERENCES role,
-	permission integer NOT NULL REFERENCES permission,
+CREATE TABLE role_permissions (
+	id uuid PRIMARY KEY,
+	role uuid NOT NULL REFERENCES role,
+	permission uuid NOT NULL REFERENCES permission,
 	UNIQUE (role, permission)
 );
 
-CREATE TABLE contact_role_map (
-	id serial PRIMARY KEY,
-	contact integer NOT NULL REFERENCES contact,
-	role integer NOT NULL REFERENCES role,
+CREATE TABLE contact (
+	id uuid PRIMARY KEY,
+	email text NOT NULL UNIQUE,
+	registered timestamp(0) with time zone NOT NULL DEFAULT now()
+);
+
+CREATE TABLE contact_roles (
+	id uuid PRIMARY KEY,
+	contact uuid NOT NULL REFERENCES contact,
+	role uuid NOT NULL REFERENCES role,
 	UNIQUE (contact, role)
 );
 
-CREATE TABLE contact_field (
-	id serial PRIMARY KEY,
+CREATE TABLE contact_group (
+	id uuid PRIMARY KEY,
 	name text NOT NULL UNIQUE
 );
 
+CREATE TABLE contact_groups (
+	id uuid PRIMARY KEY,
+	contact uuid NOT NULL REFERENCES contact,
+	contact_group uuid NOT NULL REFERENCES contact_group,
+	UNIQUE (contact, contact_group)
+);
+
+CREATE TABLE contact_field (
+	id uuid PRIMARY KEY,
+	name text NOT NULL UNIQUE,
+	datatype text NOT NULL
+);
+
 CREATE TABLE contact_field_value (
-	id serial PRIMARY KEY,
-	contact integer NOT NULL REFERENCES contact,
-	field integer NOT NULL REFERENCES contact_field,
+	id uuid PRIMARY KEY,
+	contact uuid NOT NULL REFERENCES contact,
+	field uuid NOT NULL REFERENCES contact_field,
 	data bytea,
 	UNIQUE (contact, field)
 );
