159a5b2763bca18a8620ae8fbc7a4c1540a39200
Victor van Poppelen <vvanpo@gmail.com>
2018-10-29 22:48:22 -0700

    Remove PascalCase, as postgres doesn't support it without quotes

--- schema.sql
+++ schema.sql
@@ -1,39 +1,56 @@
 
-CREATE TABLE Contact (
+CREATE TABLE contact (
 	id serial PRIMARY KEY,
 	email text NOT NULL UNIQUE,
 	name text NOT NULL,
 	registered timestamp(0) with time zone NOT NULL DEFAULT now()
 );
 
-CREATE TABLE ContactGroup (
+CREATE TABLE contact_group (
 	id serial PRIMARY KEY,
 	name text NOT NULL UNIQUE
 );
 
-CREATE TABLE ContactGroupMap (
+CREATE TABLE contact_group_map (
 	id serial PRIMARY KEY,
-	contact integer REFERENCES Contact,
-	contactGroup integer REFERENCES ContactGroup
+	contact integer NOT NULL REFERENCES contact,
+	contact_group integer NOT NULL REFERENCES contact_group,
+	UNIQUE (contact, contact_group)
 );
 
-CREATE TABLE Role (
+CREATE TABLE role (
 	id serial PRIMARY KEY,
 	name text NOT NULL UNIQUE
 );
 
-CREATE TABLE Permission (
+CREATE TABLE permission (
 	id serial PRIMARY KEY,
 	name text NOT NULL UNIQUE
 );
 
-CREATE TABLE RolePermissionMap (
+CREATE TABLE role_permission_map (
 	id serial PRIMARY KEY,
-	permission integer NOT NULL REFERENCES Permission
+	role integer NOT NULL REFERENCES role,
+	permission integer NOT NULL REFERENCES permission,
+	UNIQUE (role, permission)
 );
 
-CREATE TABLE ContactRoleMap (
+CREATE TABLE contact_role_map (
 	id serial PRIMARY KEY,
-	contact integer NOT NULL REFERENCES Contact,
-	role integer NOT NULL REFERENCES Role
+	contact integer NOT NULL REFERENCES contact,
+	role integer NOT NULL REFERENCES role,
+	UNIQUE (contact, role)
+);
+
+CREATE TABLE contact_field (
+	id serial PRIMARY KEY,
+	name text NOT NULL UNIQUE
+);
+
+CREATE TABLE contact_field_value (
+	id serial PRIMARY KEY,
+	contact integer NOT NULL REFERENCES contact,
+	field integer NOT NULL REFERENCES contact_field,
+	data bytea,
+	UNIQUE (contact, field)
 );
