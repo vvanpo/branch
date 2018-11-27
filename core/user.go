0adc91ddbc801b1e9222253d4ec979f0288c51a7
Victor van Poppelen <vvanpo@gmail.com>
2018-11-26 21:07:36 -0800

    Remove RBAC methods

--- core/user.go
+++ core/user.go
@@ -16,25 +16,3 @@ func (u User) Contact() *Contact {
 	return u.contact
 }
 
-//
-func (u User) Access(permission string) bool {
-	for _, group := range u.app.groups.list {
-		if _, ok := group.contacts[u.contact.id]; ok {
-			for _, p := range group.permissions {
-				if p == permission {
-					return true
-				}
-			}
-		}
-	}
-
-	return false
-}
-
-func (u User) AccessField(field *Field) (read, write bool) {
-	return
-}
-
-func (u User) AccessGroupField(field *Field, group *Group) (read, write bool) {
-	return
-}
