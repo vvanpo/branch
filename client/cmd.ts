2243bc259569271a8d340db9eb458c773440f2ae
Victor van Poppelen <vvanpo@gmail.com>
2018-11-27 20:40:23 -0800

    Add options to cli client

--- client/cmd.ts
+++ client/cmd.ts
@@ -1,4 +1,12 @@
-var program = require('commander');
+import {Command} from 'commander'
+
+const program = new Command()
 
 program
-    .parse(process.argv);
+    .option('-c, --config <path>', 'configuration options file')
+    .option('-H, --host <url> [port]', 'instance hostname or socket file')
+
+program
+    .command('contacts', 'find, list, or update contacts')
+
+program.parse(process.argv)
