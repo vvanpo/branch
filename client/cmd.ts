8202f168c255700c314e8b1c6701287803127e7b
Victor van Poppelen <vvanpo@gmail.com>
2018-11-29 19:30:04 -0800

    Add client commands

--- client/cmd.ts
+++ client/cmd.ts
@@ -4,11 +4,44 @@ const args = yargs
     .option("c", {
         alias: "config",
         describe: "Path to configuration file",
+        config: true,
     })
     .option("h", {
         alias: "host",
-        describe: "Instance hostname or socket file",
+        describe: "Instance hostname",
+        type: "string",
+        default: "localhost",
     })
-    .command("contacts", "Find, list, or update contacts")
+    .option("p", {
+        alias: "port",
+        describe: "Port number for server connection",
+        type: "number",
+        default: 8443,
+    })
+
+args.command({
+    command: ["contacts", "co"],
+    describe: "Find, list, or update contacts",
+    builder: yargs => {
+        const args = yargs.command({
+            command: "find",
+            describe: "Find a contact by e-mail",
+        })
+
+        args.command({
+            command: "list",
+            describe: "List contacts",
+            builder: {
+                filter: {
+                    describe: "Filter contacts",
+                },
+            },
+        })
+
+        return args
+    },
+})
+
+args.command("groups", "Enumerate or modify contact groups")
 
 const argv = args.argv
