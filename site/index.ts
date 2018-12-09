3a8617c85e73b26611447464f47567b53f0e05f4
Victor van Poppelen <vvanpo@gmail.com>
2018-12-08 18:56:37 -0800

    Hello world using typescript and optionally webpack

---
+++ site/index.ts
@@ -0,0 +1,6 @@
+console.log('Hello World')
+
+const el = document.createElement('h1')
+el.innerHTML = 'Hello world'
+
+document.body.appendChild(el)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,31 @@
+/*const clientConfig = {
+    entry: './client/index.ts',
+}*/
 
-module.exports = {
-    entry: './site/index.js',
+const siteConfig = {
+    entry: './site/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/, use: ['ts-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: ['script:src'],
+                    },
+                },
+            },
+        ],
+    },
+}
 
+module.exports = (env, argv) => {
+    return siteConfig
 }
