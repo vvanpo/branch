42e379977b7e3804d700e69238ba865256a0d9a1
Victor van Poppelen <vvanpo@gmail.com>
2018-12-10 22:24:32 -0800

    Add dynamic element type in JSX

--- site/components/button.tsx
+++ site/components/button.tsx
@@ -6,17 +6,41 @@ import { Component, Prop } from "@stencil/core"
 })
 export class Button {
 
-    @Prop() href: string
+    @Prop() public disabled: boolean
+    @Prop() public href: string
+    @Prop() public name: string
+    @Prop() public reset: boolean
+    @Prop() public value: string
 
-    render() {
+    public render() {
+        const Tag: string = this.href ? "a" : "button"
         return (
-            <button type="button">
-                <slot></slot>
-            </button>
+            <Tag {...this.attrs()}>
+                <slot />
+            </Tag>
         )
     }
 
-    private tag() {
-        return this.href ? "a" : "button"
+    // Determine button or anchor element attributes
+    private attrs() {
+        const a: any = {}
+
+        if (this.href) {
+            a.href = this.href
+        } else {
+            a.type = this.reset ? "reset" : "button"
+
+            if (this.value) {
+                a.name = this.name
+                a.type = "submit"
+                a.value = this.value
+            }
+
+            if (this.disabled) {
+                a.disabled = true
+            }
+        }
+
+        return a
     }
 }
