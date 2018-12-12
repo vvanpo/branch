a65b621623405076fb031e4b1b8fb2a1759a715e
Victor van Poppelen <vvanpo@gmail.com>
2018-12-11 22:51:15 -0800

    Remove button component

---
+++ site/components/landing-page.tsx
@@ -0,0 +1,13 @@
+import { Component, Prop } from "@stencil/core"
+
+@Component({
+    tag: "landing-page",
+    styleUrl: "landing-page.scss",
+})
+export class LandingPage {
+
+    public render() {
+        return (
+        )
+    }
+}
