1f952cecd11ee71a743d2f5200e8f8f900e9fc9d
Victor van Poppelen <vvanpo@gmail.com>
2018-12-23 22:50:43 -0800

    Remove required fields from groups

--- group/group.go
+++ group/group.go
@@ -1,22 +1,40 @@
-package titian
+package group
 
-import ()
-
-type gid id
+import (
+	"github.com/vvanpo/titian/contact"
+)
 
 type Group struct {
-	app         *Container
-	id          gid
 	name        string
 	description string
-	contacts    []*Contact
+	contacts    map[*Contact]struct{}
+}
+
+// Name returns the name of the group.
+func (g Group) Name() string {
+	return g.name
+}
+
+func (g *Group) SetName(name string) error {
+	g.name = name
+	return nil
+}
+
+// Description returns an optional description of the group.
+func (g Group) Description() string {
+	return g.description
+}
+
+func (g *Group) SetDescription(description string) error {
+	g.description = description
+	return nil
 }
 
 // Members returns an unordered list of all contacts belonging to the group.
 func (g Group) Members() []*Contact {
 	contacts := make([]*Contact, 0, len(g.contacts))
 
-	for _, contact := range g.contacts {
+	for contact := range g.contacts {
 		contacts = append(contacts, contact)
 	}
 
@@ -29,43 +47,4 @@ func (g *Group) AddContact(contact *Contact) {
 
 func (g *Group) RemoveContact(contact *Contact) {
 	delete(g.contacts, contact.id)
-}
-
-// Name returns the name of the group.
-func (g Group) Name() string {
-	return g.name
-}
-
-func (g *Group) SetName(name string) {
-	g.name = name
-}
-
-// Description returns an optional description of the group.
-func (g Group) Description() string {
-	return g.description
-}
-
-func (g *Group) SetDescription(desc string) {
-	g.description = desc
-}
-
-// RequiredFields returns a list of fields required for members of the group.
-func (g Group) RequiredFields() []*Field {
-	fields := make([]*Field, 0, len(g.requiredFields))
-
-	for _, field := range g.requiredFields {
-		fields = append(fields, field)
-	}
-
-	return fields
-}
-
-// AddRequiredField
-func (g *Group) AddRequiredField(field *Field) {
-	g.requiredFields[field.id] = field
-}
-
-// RemoveRequiredField
-func (g *Group) RemoveRequiredField(field *Field) {
-	delete(g.requiredFields, field.id)
 }
