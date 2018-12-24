3049a51187f1b917983c898d4db6da36d1e0dcb1
Victor van Poppelen <vvanpo@gmail.com>
2018-12-23 23:25:42 -0800

    Remove primary e-mail distinction

--- contact/contact.go
+++ contact/contact.go
@@ -10,76 +10,38 @@ import (
 // A Contact represents any individual or organization with a known e-mail
 // address or addresses.
 type Contact struct {
-	// email is a required field, and denotes the contact's primary e-mail
-	// address.
-	email      email.Address
-	alternates []email.Address
-	fields     map[*field.Field]field.Value
-}
-
-// EmailAddress returns the contact's primary e-mail address.
-func (c Contact) EmailAddress() email.Address {
-	return c.email
+	emails []email.Address
+	fields map[*field.Field]field.Value
 }
 
 // EmailAddresses returns a slice of all e-mail addresses associated with the
 // contact.
 func (c Contact) EmailAddresses() []email.Address {
-	return append([]email.Address{c.email}, c.alternates...)
+	return c.email[:]
 }
 
-// AddEmailAddress adds an e-mail address to the contact's e-mail list. Returns
-// an error for non-unique e-mails, and will panic if passed the zero value.
-// The passed e-mail address is assumed to be verified. A verified e-mail
-// address is one that the application has received an e-mail from in the past,
-// was provided by a third-party authentication provider, or has been manually
-// set as verified. A contact can have multiple associated e-mail addresses if
-// they have been linked together (e.g. by the owning user, or by an
-// administrator).
+// AddEmailAddress adds an e-mail address to the contact's e-mail list. Will
+// panic if passed the zero value.
 func (c *Contact) AddEmailAddress(address email.Address) error {
 	if address == (email.Address{}) {
 		panic("Attempting to add an invalid e-mail address")
 	}
 
-	c.alternates = append(c.alternates, address)
+	c.email = append(c.email, address)
 	return nil
 }
 
-// SetPrimaryEmailAddress sets the primary e-mail address for the contact,
-// bumping the existing primary e-mail address onto the list of alternate
-// addresses. If the passed e-mail address does not already belong to the
-// contact, it is assumed to be verified. Will panic if passed a zero-value.
-func (c *Contact) SetPrimaryEmailAddress(address email.Address) error {
-	emails := append([]email.Address{c.email}, c.alternates...)
-
-	for i, e := range emails {
-		if e == address {
-			c.email = address
-			c.alternates = append(emails[:i], emails[i+1:]...)
-			return nil
-		}
+// RemoveEmailAddress removes an e-mail address from the contact. Panics if
+// removing the address would leave the contact without any e-mail addresses.
+// Passing an e-mail address not belonging to the contact is a no-op.
+func (c *Contact) RemoveEmailAddress(address email.Address) error {
+	if len(c.emails) <= 1 {
+		panic("Cannot remove all e-mail addresses from contact")
 	}
 
-	if err := c.AddEmailAddress(address); err != nil {
-		return err
-	}
-
-	c.email = address
-	c.alternates = emails[:len(emails)-1]
-	return nil
-}
-
-// RemoveEmailAddress removes a secondary e-mail address. Returns an error if
-// the passed e-mail is the contact's primary address. Passing an e-mail address
-// not belonging to the contact is a no-op.
-func (c *Contact) RemoveEmailAddress(email email.Address) error {
-	if email == c.email {
-		return errors.New("Cannot remove your primary e-mail address")
-	}
-
-	for i, e := range c.alternates {
-		if email == e {
-			c.alternates = append(c.alternates[:i], c.alternates[i+1:]...)
+	for i, a := range c.emails {
+		if a == address {
+			c.emails = append(c.emails[:i], c.emails[i+1:]...)
 			return nil
 		}
 	}
@@ -114,6 +76,7 @@ func (c *Contact) SetField(field *field.Field, value string) error {
 	return nil
 }
 
+// DeleteField
 func (c *Contact) DeleteField(field *field.Field) {
 	delete(c.fields, field)
 }
@@ -123,7 +86,7 @@ func (c *Contact) DeleteField(field *field.Field) {
 func (c *Contact) Merge(associated *Contact) {
 	emails := associated.EmailAddresses()
 	fields := associated.fields
-	c.alternates = append(c.alternates, emails...)
+	c.emails = append(c.emails, emails...)
 
 	for field, value := range fields {
 		if _, ok := c.fields[field]; !ok {
