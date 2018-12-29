a6472775334502bfc07a50466cedef038e641527
Victor van Poppelen <vvanpo@gmail.com>
2018-12-28 19:06:31 -0800

    Transform contact email list to unordered set

--- contact/contact.go
+++ contact/contact.go
@@ -1,6 +1,8 @@
 package contact
 
 import (
+	"sort"
+
 	"github.com/vvanpo/titian/email"
 	"github.com/vvanpo/titian/field"
 )
@@ -8,51 +10,52 @@ import (
 // A Contact represents any individual or organization with a known e-mail
 // address or addresses.
 type Contact struct {
-	emails []email.Address
+	emails map[email.Address]struct{}
 	fields map[*field.Field]field.Value
 }
 
 // EmailAddresses returns a slice of all e-mail addresses associated with the
 // contact.
 func (c Contact) EmailAddresses() []email.Address {
-	return c.emails[:]
-}
+	addresses := make([]string, len(c.emails))
 
-// AddEmailAddress adds an e-mail address to the contact's e-mail list. Will
-// panic if passed the zero value.
-func (c *Contact) AddEmailAddress(address email.Address) error {
-	if address == (email.Address{}) {
-		panic("Attempting to add an invalid e-mail address")
+	for address := range c.emails {
+		addresses = append(addresses, address)
 	}
 
-	c.emails = append(c.emails, address)
-	return nil
+	sort.Strings(addresses)
+	return addresses
+}
+
+// AddEmailAddress adds an e-mail address or addresses to the contact's e-mail
+// list. Will panic if passed the zero value.
+func (c *Contact) AddEmailAddress(addresses ...email.Address) {
+	for _, address := range addresses {
+		if address == (email.Address{}) {
+			panic("Attempting to add an invalid e-mail address")
+		}
+
+		c.emails[address] = struct{}{}
+	}
 }
 
 // RemoveEmailAddress removes an e-mail address from the contact. Panics if
 // removing the address would leave the contact without any e-mail addresses.
 // Passing an e-mail address not belonging to the contact is a no-op.
-func (c *Contact) RemoveEmailAddress(address email.Address) error {
+func (c *Contact) RemoveEmailAddress(address email.Address) {
 	if len(c.emails) <= 1 {
 		panic("Cannot remove all e-mail addresses from contact")
 	}
 
-	for i, a := range c.emails {
-		if a == address {
-			c.emails = append(c.emails[:i], c.emails[i+1:]...)
-			return nil
-		}
-	}
-
-	return nil
+	delete(c.emails, address)
 }
 
-// Fields
-func (c Contact) Fields() []*field.Field {
-	fields := make([]*field.Field, len(c.fields))
+// Fields returns a map of all field values.
+func (c Contact) Fields() map[*field.Field]string {
+	fields := make(map[*field.Field]string, len(c.fields))
 
-	for field := range c.fields {
-		fields = append(fields, field)
+	for field, value := range c.fields {
+		fields[field] = value.String()
 	}
 
 	return fields
@@ -74,19 +77,19 @@ func (c *Contact) SetField(field *field.Field, value string) error {
 	return nil
 }
 
-// DeleteField
+// DeleteField deletes the value associated with the passed field.
 func (c *Contact) DeleteField(field *field.Field) {
 	delete(c.fields, field)
 }
 
-// Merge combines two contacts. The receiver retains its fields and primary
-// e-mail address, and additional fields from the associated contact are added.
+// Merge combines two contacts. The receiver retains its fields while additional
+// fields from the associated contact are added.
 func (c *Contact) Merge(associated *Contact) {
-	emails := associated.EmailAddresses()
-	fields := associated.fields
-	c.emails = append(c.emails, emails...)
+	for address := range associated.EmailAddresses() {
+		c.AddEmailAddress
+	}
 
-	for field, value := range fields {
+	for field, value := range associated.fields {
 		if _, ok := c.fields[field]; !ok {
 			c.fields[field] = value
 		}
